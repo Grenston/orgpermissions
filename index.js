355562a61b63c7f4e84c192cb55052fc57d2b8b5
grenstong <grenston.george@ecanarys.com>
2021-07-24 16:38:54 +0530

    started index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+const readline = require("readline");
+const { Octokit } = require("@octokit/rest");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+console.log('AUTOMATE ORGS');
+rl.question("Enter your pat: ", function(GH_PAT) {
+        const github = new Octokit({
+            auth: GH_PAT
+          });
+        rl.close();
+    });
+
+rl.on("close", function() {
+    console.log("\n----------------------------------------------");
+    console.log("\n**********Developed by Grenston !!!***********");
+    console.log("\n----------------------------------------------");
+    process.exit(0);
+});
