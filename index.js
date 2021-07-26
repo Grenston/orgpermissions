a24c1e521901459bb72f14d9e184888357854bf5
grenstong <grenston.george@ecanarys.com>
2021-07-26 19:33:25 +0530

    config file added

--- index.js
+++ index.js
@@ -1,20 +1,37 @@
 const readline = require("readline");
 const { Octokit } = require("@octokit/rest");
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-console.log('AUTOMATE ORGS');
-rl.question("Enter your pat: ", function(GH_PAT) {
-    const github = new Octokit({
-        auth: GH_PAT
+const config = require('./config.json');
+async function set_rules(){
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
     });
-    rl.close();
-});
+    console.log('AUTOMATE ORGS');
+    rl.question("Enter your pat: ", async function(GH_PAT) {
+        const github = new Octokit({
+            auth: GH_PAT
+        });
+        config.orgnames.forEach(async org => {
+            await github.request('PATCH /orgs/{org}', {
+                org: org,
+                billing_email: 'grenston.george@ecanarys.com'
+            })       
+            .then(response => {
+                console.log(response)
+            })
+            .catch(error => {
+                console.log(error)
+            });
+        });
+        rl.close();
+    });
 
-rl.on("close", function() {
-    console.log("\n----------------------------------------------");
-    console.log("\n**********Developed by Grenston !!!***********");
-    console.log("\n----------------------------------------------");
-    process.exit(0);
-});
+    rl.on("close", function() {
+        console.log("\n----------------------------------------------");
+        console.log("\n**********Developed by Grenston !!!***********");
+        console.log("\n----------------------------------------------");
+        process.exit(0);
+    });
+}
+set_rules()
+//Access token grenstong: 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
